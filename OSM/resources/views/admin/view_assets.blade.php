486ac68580a121a04523918bb190825be4931d9a
TruptiShetmandrekar <mca.1954@unigoa.ac.in>
2021-05-13 00:21:18 +0530

    admin

--- OSM/resources/views/admin/view_assets.blade.php
+++ OSM/resources/views/admin/view_assets.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.masterAdmin')
-
 @section('content')
 <div class="row">
 <div class="col-lg-12 ">
     <div class="card">
       <div class="card-body">
-<div><a class="btn btn-primary " href="{{ url('/admin/add_technician') }}">Add Technician</a>
+<div><a class="btn btn-primary " href="{{ url('/admin/add_asset') }}">Add New Product</a>
 </div>
 </div>
 </div>
@@ -13,41 +12,44 @@
   <div class="col-lg-12 ">
     <div class="card">
       <div class="card-body">
-        <h4 class="card-title">Technician Details</h4>
+        <h4 class="card-title">Product Details</h4>
         <div class="table-responsive">
           <table class="table table-striped">
           <thead>
               <tr>
                 <th> # </th>
-                <th> Technician id </th>
-                <th> Technician Name </th>
-                <th> Technician City </th>
-                <th> Technician Mobile </th>
-                <th> Technician Email </th>
+                <th> Product id </th>
+                <th> Product Name </th>
+                <th> Date of purchase </th>
+                <th> Avaliable </th>
+                <th> Total </th>
+                <th> cost </th>
+                <th> selling price </th>
                 <th> Action </th>
               </tr>
             </thead>
             <tbody>
             
             <?php  $cnt=1;?>
-            @foreach($technician_tbs as $technician_tb)
+            @foreach($assets_tbs as $assets_tb)
               <tr>
                 <td> <?php echo $cnt;?></td>
-                <td> {{$technician_tb['empid']}} </td>
-                <td>{{$technician_tb['empName']}}</td>
-                <td>{{$technician_tb['empCity']}}</td>
-                <td>{{$technician_tb['empMobile']}}</td>
-                <td>{{$technician_tb['empEmail']}} </td>
+                <td> {{$assets_tb['pid']}} </td>
+                <td>{{$assets_tb['pname']}}</td>
+                <td>{{$assets_tb['pdop']}}</td>
+                <td>{{$assets_tb['pava']}}</td>
+                <td>{{$assets_tb['ptotal']}} </td>
+                <td>{{$assets_tb['poriginalcost']}} </td>
+                <td>{{$assets_tb['psellingcost']}} </td>
                 <td>
                 <form action="editemp.php" method="POST"> 
-                <input type="hidden" name="id" value='. $row["empid"] .'>
+                <input type="hidden" name="id" value='. $row["pid"] .'>
                 <button type="submit" class="btn btn-primary" href="" name="view" >edit</button>
                 @csrf
                 @method('DELETE')
-                
                 </form></td>
                 <td>
-                  <a href="/click_delete_technician/{{ $technician_tb['empid'] }}" class="btn btn-danger"> Delete </a>
+                  <a href="/click_delete_Product/{{ $assets_tb['pid'] }}" class="btn btn-danger"> Delete </a>
                 </td>
               </tr>
               <?php 
